68c1e76eb58fca1faaa6cee9775328b210210dd5
bajadda1 <2143ukeuqxbb0izv5hnguqgt73dfbe@bots.bitbucket.org>
2025-08-21 17:01:52 +0100

    Headless storefronts added

---
+++ hybris/bin/custom/myabrasives_headless_nor/src/app/spartacus/custom/my-pdp-custom/my-pdp-custom.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-pdp-custom',
+  standalone: false,
+  templateUrl: './my-pdp-custom.component.html',
+  styleUrl: './my-pdp-custom.component.scss'
+})
+export class MyPdpCustomComponent {
+
+}
